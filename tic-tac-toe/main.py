ae27a3dacb0a8bbf1ed0218c94f6b85c2ef311c2
Jezuke <jezaki48@gmail.com>
2023-08-20 21:23:01 -0700

    init grid

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -1,33 +1,51 @@
 import pygame
+import sys
 
+# TODO clean up Grid by closing borders and centering grid at a smaller scale
+
+# Initialize Pygame
 pygame.init()
-screen = pygame.display.set_mode((1280,720))
-clock = pygame.time.Clock()
-running = True
-dt = 0
 
-player_pos = pygame.Vector2(screen.get_width()/2, screen.get_height()/2)
+# Constants
+SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
+GRID_SIZE = 3
+CELL_SIZE = min(SCREEN_WIDTH, SCREEN_HEIGHT) // GRID_SIZE
+
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+# Calculate grid position to center it
+grid_x = (SCREEN_WIDTH - CELL_SIZE * GRID_SIZE) // 2
+grid_y = (SCREEN_HEIGHT - CELL_SIZE * GRID_SIZE) // 2
+
+# Create the display
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Centered 3x3 Grid")
+
+# Main loop
+running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    
-    screen.fill("black")
 
-    pygame.draw.circle(screen, "white", player_pos, 40)
+    # Clear the screen
+    screen.fill(WHITE)
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
-    
+    # Draw vertical lines
+    for x in range(0, GRID_SIZE):
+        pygame.draw.line(screen, BLACK, (grid_x + x * CELL_SIZE, grid_y),
+                         (grid_x + x * CELL_SIZE, grid_y + CELL_SIZE * GRID_SIZE), 2)
+
+    # Draw horizontal lines
+    for y in range(0, GRID_SIZE):
+        pygame.draw.line(screen, BLACK, (grid_x, grid_y + y * CELL_SIZE),
+                         (grid_x + CELL_SIZE * GRID_SIZE, grid_y + y * CELL_SIZE), 2)
+
+    # Update the display
     pygame.display.flip()
 
-    dt = clock.tick(60)/1000
-
+# Quit Pygame
 pygame.quit()
+sys.exit()
