cb7e5688021a66a49e5d1e8067ec2bc0563285ac
Jezuke <jezaki48@gmail.com>
2023-08-21 14:28:30 -0700

    Simplified grid drawing. Need to figure out how to draw based on click

--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 import sys
 
 # TODO clean up Grid by closing borders and centering grid at a smaller scale
@@ -7,45 +8,64 @@ import sys
 pygame.init()
 
 # Constants
-SCREEN_WIDTH, SCREEN_HEIGHT = 1280, 720
-GRID_SIZE = 3
-CELL_SIZE = min(SCREEN_WIDTH, SCREEN_HEIGHT) // GRID_SIZE
+SCREEN_WIDTH, SCREEN_HEIGHT = 750, 750
+CELL_SIZE = 150
+CENTER_CELL_X = (SCREEN_WIDTH-CELL_SIZE)//2
+CENTER_CELL_Y = (SCREEN_HEIGHT-CELL_SIZE)//2
 
 # Colors
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
-# Calculate grid position to center it
-grid_x = (SCREEN_WIDTH - CELL_SIZE * GRID_SIZE) // 2
-grid_y = (SCREEN_HEIGHT - CELL_SIZE * GRID_SIZE) // 2
-
 # Create the display
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Centered 3x3 Grid")
+font = pygame.font.Font(size=32)
+
+# Functions
+def print_grid():
+    for x in range(math.ceil(SCREEN_WIDTH/CELL_SIZE)):
+        for y in range(math.ceil(SCREEN_HEIGHT/CELL_SIZE)):
+            pygame.draw.line(screen, BLACK, (x*CELL_SIZE, CELL_SIZE), (x*CELL_SIZE,SCREEN_HEIGHT-CELL_SIZE))
+            pygame.draw.line(screen, BLACK, (CELL_SIZE, y*CELL_SIZE), (SCREEN_WIDTH-CELL_SIZE,y*CELL_SIZE))
+   
+            text = font.render(str((x,y)),True, BLACK, WHITE)
+            textRect = text.get_rect()
+            textRect.center = (x*CELL_SIZE+(CELL_SIZE/2), y*CELL_SIZE+(CELL_SIZE/2))
+            screen.blit(text, textRect)
 
 # Main loop
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+def main():
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    # Clear the screen
-    screen.fill(WHITE)
+        # Clear the screen
+        screen.fill(WHITE)
+        
+        print_grid()
 
-    # Draw vertical lines
-    for x in range(0, GRID_SIZE):
-        pygame.draw.line(screen, BLACK, (grid_x + x * CELL_SIZE, grid_y),
-                         (grid_x + x * CELL_SIZE, grid_y + CELL_SIZE * GRID_SIZE), 2)
+        """
+            if user clicks on box:
+                if box is empty:
+                    if state is 'x':
+                        render 'x' in box
+                    else:
+                        render 'o' in box
+            
+            if winning state:
+                end game declare winner
+        """
+        # Update the display
+        pygame.display.flip()
 
-    # Draw horizontal lines
-    for y in range(0, GRID_SIZE):
-        pygame.draw.line(screen, BLACK, (grid_x, grid_y + y * CELL_SIZE),
-                         (grid_x + CELL_SIZE * GRID_SIZE, grid_y + y * CELL_SIZE), 2)
+    # Quit Pygame
+    pygame.quit()
+    sys.exit()
 
-    # Update the display
-    pygame.display.flip()
+if __name__ == "__main__":
+    main()
 
-# Quit Pygame
-pygame.quit()
-sys.exit()
+
