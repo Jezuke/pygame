bc0ec68c0ededeb7648bfb604e3c8798f9ea85bf
Jezuke <jezaki48@gmail.com>
2023-08-25 10:51:25 -0700

    Fin.

--- test.py
+++ test.py
@@ -1,25 +1,18 @@
-import sys, pygame
-pygame.init()
+markers_x_set = {(1, 2), (2, 1), (3, 1), (2, 2), (1, 3)}
+wins = [
+    # Horizontal wins
+    {(1,1),(2,1),(3,1)},
+    {(1,2), (2,2),(3,2)},
+    {(1,3), (2,3), (3,3)},
+    # Vertical wins
+    {(1,1),(1,2),(1,3)},
+    {(2,1),(2,2),(2,3)},
+    {(3,1),(3,2),(3,3)},
+    # Diagonal wins
+    {(1,1),(2,2),(3,3)},
+    {(1,3),(2,2),(3,1)}
+]
 
-size = width, height = 1280,720
-speed = [1, 1]
-black = 0, 0, 0
-
-screen = pygame.display.set_mode(size)
-
-ball = pygame.image.load("intro_ball.gif")
-ballrect = ball.get_rect()
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
-
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
-
-    screen.fill(black)
-    screen.blit(ball, ballrect)
-    pygame.display.flip() 
+for win in wins:
+    x_win = win.issubset(markers_x_set)
+    print(x_win)
--- tic-tac-toe/main.py
+++ tic-tac-toe/main.py
@@ -2,24 +2,32 @@ import pygame
 import math
 import sys
 
-"""
-TODO:
-* Scale reset button
-* Add reset button functionality
-"""
-
 # Initialize Pygame
 pygame.init()
 
 # Button class
 class Button:
-    def __init__(self, x, y, image) -> None:
-        self.image = image
+    def __init__(self, x, y, image, scale=1) -> None:
+        width = image.get_width()
+        height = image.get_height()
+        self.image = pygame.transform.scale(image, (int(width*scale), int(height*scale)))
         self.rect = self.image.get_rect()
         self.rect.center = (x,y)
 
     def draw(self):
-        screen.blit(self.image, (self.rect.x, self.rect.y))
+        global clicked
+        pos = pygame.mouse.get_pos()
+
+        if self.rect.collidepoint(pos):
+            inv = pygame.Surface(self.image.get_size())
+            inv.fill((255,255,255))
+            inv.blit(self.image, (0,0), None, pygame.BLEND_RGB_SUB)
+            screen.blit(inv, (self.rect.x, self.rect.y))
+            
+            if pygame.mouse.get_pressed()[0] == True:
+                reset_state()
+        else:
+            screen.blit(self.image, (self.rect.x, self.rect.y))
 
 # Constants
 SCREEN_WIDTH, SCREEN_HEIGHT = 750, 750
@@ -46,10 +54,13 @@ BLACK = (0, 0, 0)
 
 # Create the display
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("Centered 3x3 Grid")
+pygame.display.set_caption("Ticcer-Taccer-Toer")
 font = pygame.font.Font(size=32)
 reset_img = pygame.image.load("img/reset_button.png").convert_alpha()
-reset_button = Button(100,100, reset_img)
+
+# RIP the magic numbers, but yea the 2 and 4 refer to cell (2,4) scaled by the cell_size and centered in the cell.
+reset_button = Button(2*CELL_SIZE+(CELL_SIZE/2),4*CELL_SIZE+(CELL_SIZE/2), reset_img)
+
 # Global variables
 clicked = False
 markers_x = []
@@ -58,6 +69,19 @@ player_turn = True # True means 'X' False means 'O'
 winner = None
 
 # Functions
+def reset_state():
+    global clicked
+    global markers_x
+    global markers_o
+    global player_turn
+    global winner
+
+    clicked = False
+    markers_x = []
+    markers_o = []
+    player_turn = True
+    winner = None
+
 def print_grid():
     for x in range(math.ceil(SCREEN_WIDTH/CELL_SIZE)):
         for y in range(math.ceil(SCREEN_HEIGHT/CELL_SIZE)):
@@ -125,16 +149,18 @@ def check_winner():
             
             x_win = win.issubset(markers_x_set)
             o_win = win.issubset(markers_o_set)
+            
             if x_win:
                 winner = 'X'
                 return True
             elif o_win:
                 winner = 'O'
                 return True
-            elif len(markers_x)+len(markers_o) == 9 and not x_win: # Scuffed, but implies Draw since all squares filled up
-                winner = 'DRAW. NOBODY'
-                return True
- 
+
+        number_of_turns = len(markers_x)+len(markers_o)
+        if number_of_turns == 9:
+            winner = 'DRAW. NOBODY'
+            return True
 
 def game_over(winner):
     string_text = "GAME OVER: " + winner +" WINS!"
